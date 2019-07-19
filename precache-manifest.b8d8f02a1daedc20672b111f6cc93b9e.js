8b84c51c96c82afbf0f71ac803f93db55e490c3e
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-19 21:36:19 +0300

    Updates

---
+++ precache-manifest.b8d8f02a1daedc20672b111f6cc93b9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0e9fa47d3a60f93695655a538e8ed0",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "225f0cc2fb7db45d0fd1",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.39017165.chunk.css"
+  },
+  {
+    "revision": "d249de78ebb518ee5f23",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.47a378b0.chunk.css"
+  },
+  {
+    "revision": "225f0cc2fb7db45d0fd1",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.68666373.chunk.js"
+  },
+  {
+    "revision": "d249de78ebb518ee5f23",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.9e045ca8.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
